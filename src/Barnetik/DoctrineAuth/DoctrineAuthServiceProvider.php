1e6b3434dcc6f79ab0d70f2faf55113b3a0e8a51
Arkaitz Etxeberria <arkaitz@barnetik.com>
2015-03-23 13:59:21 +0100

    Check doctrine configuration before call it

--- src/Barnetik/DoctrineAuth/DoctrineAuthServiceProvider.php
+++ src/Barnetik/DoctrineAuth/DoctrineAuthServiceProvider.php
@@ -1,54 +1,58 @@
-<?php namespace Barnetik\DoctrineAuth;
+<?php
+
+namespace Barnetik\DoctrineAuth;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 
-class DoctrineAuthServiceProvider extends ServiceProvider {
+class DoctrineAuthServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
-    
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
     public function boot()
     {
-        $em = $this->app->make('Doctrine\ORM\EntityManager');
-        $driver = \Doctrine\ORM\Mapping\Driver\AnnotationDriver::create(__DIR__);
-        
-        $driverChain = $em->getConfiguration()->getMetadataDriverImpl();
-        $driverChain->addDriver($driver, 'Barnetik\DoctrineAuth');
-        
+        if (\Config::get('doctrine')) {
+            $em = $this->app->make('Doctrine\ORM\EntityManager');
+            $driver = \Doctrine\ORM\Mapping\Driver\AnnotationDriver::create(__DIR__);
+
+            $driverChain = $em->getConfiguration()->getMetadataDriverImpl();
+            $driverChain->addDriver($driver, 'Barnetik\DoctrineAuth');
+        }
         Auth::extend('doctrine', function($app) {
             $provider = new DoctrineUserProvider($app->make('Doctrine\ORM\EntityManager'), config('auth.model'));
             return new \Illuminate\Auth\Guard($provider, $app['session.store']);
         });
 
         $this->publishes([
-            __DIR__.'/../../doctrine-migrations' => base_path('/database/doctrine-migrations'),
+            __DIR__ . '/../../doctrine-migrations' => base_path('/database/doctrine-migrations'),
         ]);
-        
+
         $this->commands([
             'Barnetik\DoctrineAuth\Console\Commands\CreateUser'
         ]);
